907512d30414ac2af39e744ad7aa497b89f4cfb1
Shreyas <skraman1999@gmail.com>
2014-08-22 20:06:52 -0700

    Best object NA.

--- main.js
+++ main.js
@@ -1,18 +1,20 @@
 var game = new Phaser.Game(600, 480, Phaser.AUTO, 'gameDiv');
 
+var player = { x:"50", y:"50" };
+
 var mainState = {
 
-    preload: function() {
-        game.stage.backgroundColor = '#ccaaaa';
-        game.load.image('circle', 'assets/circle.png');
-    },
+  preload: function() {
+    game.stage.backgroundColor = '#ccaaaa';
+    game.load.image('circle', 'assets/circle.png');
+  },
 
-    create: function() {
-        this.circle = this.game.add.sprite(64, 64, 'circle');
-    },
+  create: function() {
+    this.circle = this.game.add.sprite(player.x, player.y, 'circle');
+  },
 
-    update: function() {
-    },
+  update: function() {
+  },
 };
 
 game.state.add('main', mainState);
