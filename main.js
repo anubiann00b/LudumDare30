7edcbd7b8e17a8a0127d4105852a7a0f54dd0687
Shreyas <skraman1999@gmail.com>
2014-08-23 18:02:37 -0700

    Platforms/blocks.

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ var game = new Phaser.Game(600, 480, Phaser.AUTO, 'gameDiv');
 var player1 = { x:"50", y:"50" };
 var player2 = { x:"50", y:"50" };
 var circle;
+var blocks;
 
 var mainState = {
 
@@ -13,33 +14,43 @@ var mainState = {
 
   create: function() {
     game.physics.startSystem(Phaser.Physics.ARCADE);
-    game.physics.arcade.gravity.y=300;
+
+    platforms = game.add.group();
+    platforms.enableBody = true;
+    var ledge = platforms.create(400, 400, 'circle');
+    ledge.body.immovable = true;
+    ledge = platforms.create(150, 250, 'circle');
+    ledge.body.immovable = true;
+
     this.circle = this.game.add.sprite(player1.x, player1.y, 'circle');
-
     game.physics.enable(this.circle, Phaser.Physics.ARCADE);
-    cursors = game.input.keyboard.createCursorKeys();
-
     this.circle.position.x = 10;
     this.circle.position.y = 100;
+    this.circle.body.gravity.y=300;
     this.circle.body.bounce.y = 0.2;
     this.circle.body.maxVelocity.x = 300;
-    this.circle.body.drag.x = 100;
+    this.circle.body.drag.x = 400;
     this.circle.body.collideWorldBounds = true;
     this.circle.body.setSize(30, 30, 15, 15);
 
-    this.game.input.keyboard.addKey(Phaser.Keyboard.W).onDown.add((function (){ this.circle.body.velocity.y-=300; }), this);
+    cursors = game.input.keyboard.createCursorKeys();
+    this.game.input.keyboard.addKey(Phaser.Keyboard.W).onDown.add(this.jump, this);
   },
 
   update: function() {
     kb = game.input.keyboard;
 
     if (kb.isDown(Phaser.Keyboard.D)) {
-      this.circle.body.velocity.x += 5;
+      this.circle.body.velocity.x += 50;
     }
     if (kb.isDown(Phaser.Keyboard.A)) {
-      this.circle.body.velocity.x -= 5;
+      this.circle.body.velocity.x -= 50;
     }
   },
+
+  jump: function() {
+    this.circle.body.velocity.y-=200;
+  }
 };
 
 game.state.add('main', mainState);
